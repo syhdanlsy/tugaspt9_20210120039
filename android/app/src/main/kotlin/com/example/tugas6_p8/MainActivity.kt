c826755b077356872c7210141a8412872c3513d0
syhdanlsy <msyahdanz08@gmail.com>
2024-01-08 11:52:16 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/tugas6_p8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tugas6_p8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/UserService.dart
@@ -0,0 +1,25 @@
+
+import 'package:tugas6_p8/dbhelper/repository.dart';
+import 'package:tugas6_p8/model/user.dart';
+class UserService
+{
+late Repository _repository;
+UserService(){
+_repository = Repository();
+}
+//Save User
+SaveUser(User user) async{
+return await _repository.insertData('users', user.userMap());
+}
+//Read All Users
+readAllUsers() async{
+return await _repository.readData('users');
+}
+//Edit User
+UpdateUser(User user) async{
+return await _repository.updateData('users', user.userMap());
+}
+deleteUser(userId) async {
+return await _repository.deleteDataById('users', userId);
+}
+}
